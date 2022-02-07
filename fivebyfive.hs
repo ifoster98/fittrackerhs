536c0764987a1e91343e7e41eb3704c4483cf38d
Ian Foster <ianfoster@Ians-Mac-mini.local>
2022-02-08 04:56:39 +1100

    Initial commit

---
+++ fivebyfive.hs
@@ -0,0 +1,62 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module FiveByFive where
+
+import Domain
+import Repository
+
+getExercisesForWorkout :: WorkoutType -> WorkoutSubType -> [ExerciseType]
+getExercisesForWorkout FiveByFive WorkoutA = [Squat, BenchPress, BentOverRows]
+getExercisesForWorkout FiveByFive WorkoutB = [Squat, OverheadPress, Deadlift]
+
+inc :: ExerciseType -> Weight -> Double
+inc Deadlift w = w + 5
+inc _ w = w + 2.5
+
+dec :: ExerciseType -> Weight -> Double
+dec Deadlift w = w - 5
+dec _ w = w - 2.5
+
+calculateNextWeight :: ExerciseType -> Weight -> (Outcome, Outcome) -> Double
+calculateNextWeight et w (Failure, Failure) = dec et w
+calculateNextWeight et w (_, Failure) = w
+calculateNextWeight et w (_, _) = inc et w
+
+takeLast :: Int -> [a] -> [a]
+takeLast n aList = reverse (take n (reverse aList))
+
+getWeight :: Exercise -> Double
+getWeight exercise = weight (head (sets exercise))
+
+getOutcome :: Exercise -> Outcome
+getOutcome exercise = outcome (head (sets exercise))
+
+getLastWeight :: [Exercise] -> Double
+getLastWeight ers = getWeight (last ers)
+
+getLastTwoOutcomes :: [Exercise] -> (Outcome, Outcome)
+getLastTwoOutcomes ers = (lastButOne, last)
+  where last = getOutcome (head (takeLast 1 ers))
+        lastButOne = getOutcome (head (takeLast 2 ers))
+
+generateRep :: Weight -> RepCount -> Outcome -> Reps
+generateRep w r o = Reps {weight = w, repCount = r, outcome = o}
+
+generateRepsForNextWorkout :: WorkoutType -> Weight -> [Reps]
+generateRepsForNextWorkout FiveByFive w = replicate 5 (generateRep w 5 Failure)
+
+getNextWeight :: ExerciseType -> Double
+getNextWeight et = calculateNextWeight et lastWeight lastOutcomes
+  where results = getExercises et
+        lastWeight = getLastWeight results
+        lastOutcomes = getLastTwoOutcomes results
+
+generateExercisesForNextWorkout :: WorkoutType -> WorkoutSubType -> [ExerciseType] -> [Exercise]
+generateExercisesForNextWorkout FiveByFive wst [] = []
+generateExercisesForNextWorkout FiveByFive wst (x:xs) = Exercise {exerciseType = x, exerciseTime = Nothing, sets = reps}:generateExercisesForNextWorkout FiveByFive wst xs
+  where nextWeight = getNextWeight x
+        reps = generateRepsForNextWorkout FiveByFive nextWeight
+
+generateNextWorkout :: WorkoutType -> WorkoutSubType -> Workout
+generateNextWorkout wt wst = Workout { workoutType = wt, workoutSubType = wst, workoutTime = Nothing, exercises = exercises }
+  where exercisesForWorkout = getExercisesForWorkout wt wst
+        exercises = generateExercisesForNextWorkout wt wst exercisesForWorkout
---
+++ repository.hs
@@ -0,0 +1,48 @@
+module Repository where
+
+import Domain
+import Data.Time.Calendar
+import Data.Time
+
+saveActualWorkout :: Workout -> IO Bool
+saveActualWorkout wkt = pure True
+
+saveProposedWorkout :: Workout -> IO Bool
+saveProposedWorkout wkt = pure True
+
+
+exerciseDay1 = UTCTime (fromGregorian 2022 02 01) (secondsToDiffTime 0)
+exerciseDay2 = UTCTime (fromGregorian 2022 02 03) (secondsToDiffTime 0)
+exerciseDay3 = UTCTime (fromGregorian 2022 02 05) (secondsToDiffTime 0)
+
+getReps :: Double -> [Reps]
+getReps w = replicate 5 r
+  where
+    r = Reps {weight = w, repCount = 5, outcome = Success}
+
+s1 = Exercise {exerciseType = Squat, exerciseTime = Just exerciseDay1, sets = getReps 50}
+s2 = Exercise {exerciseType = Squat, exerciseTime = Just exerciseDay2, sets = getReps 52.5}
+s3 = Exercise {exerciseType = Squat, exerciseTime = Just exerciseDay3, sets = getReps 55}
+
+d1 = Exercise {exerciseType = Deadlift, exerciseTime = Just exerciseDay1, sets = getReps 70}
+d2 = Exercise {exerciseType = Deadlift, exerciseTime = Just exerciseDay2, sets = getReps 75}
+d3 = Exercise {exerciseType = Deadlift, exerciseTime = Just exerciseDay3, sets = getReps 80}
+
+b1 = Exercise {exerciseType = BenchPress, exerciseTime = Just exerciseDay1, sets = getReps 50}
+b2 = Exercise {exerciseType = BenchPress, exerciseTime = Just exerciseDay2, sets = getReps 52.5}
+b3 = Exercise {exerciseType = BenchPress, exerciseTime = Just exerciseDay3, sets = getReps 55}
+
+o1 = Exercise {exerciseType = OverheadPress, exerciseTime = Just exerciseDay1, sets = getReps 30}
+o2 = Exercise {exerciseType = OverheadPress, exerciseTime = Just exerciseDay2, sets = getReps 32.5}
+o3 = Exercise {exerciseType = OverheadPress, exerciseTime = Just exerciseDay3, sets = getReps 35}
+
+br1 = Exercise {exerciseType = BentOverRows, exerciseTime = Just exerciseDay1, sets = getReps 40}
+br2 = Exercise {exerciseType = BentOverRows, exerciseTime = Just exerciseDay2, sets = getReps 42.5}
+br3 = Exercise {exerciseType = BentOverRows, exerciseTime = Just exerciseDay3, sets = getReps 45}
+
+getExercises :: ExerciseType -> [Exercise]
+getExercises Squat = [s1, s2, s3]
+getExercises Deadlift = [d1, d2, d3]
+getExercises BenchPress = [b1, b2, b3]
+getExercises OverheadPress = [o1, o2, o3]
+getExercises BentOverRows = [br1, br2, br3]

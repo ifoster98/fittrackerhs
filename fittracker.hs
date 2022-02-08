7a63cc59e70f0ffddac11c26f276c66595259068
Ian Foster <ianfoster@Ians-Mac-mini.local>
2022-02-08 16:11:11 +1100

    Adding details of what is needed in implementation of top-level functions

--- fittracker.hs
+++ fittracker.hs
@@ -6,6 +6,10 @@ import InMemRepository
 
 getNextWorkout :: WorkoutType -> WorkoutSubType -> Workout
 getNextWorkout wt wst = Workout {workoutType = wt, workoutSubType = wst, workoutTime = Nothing, exercises = []}
+-- Call repository to get next workout in sequence
 
 saveWorkout :: Workout -> Either [Error] Outcome
 saveWorkout w = Right Success
+-- Write incoming messag to log
+-- Save each exercise in the incoming workout separately to the repository
+-- Generate the next workout in this sequence and save to repository

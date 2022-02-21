07d1c495779c7d94b44cd128164610681201b9e3
Ian Foster <ianfoster@Ians-Mac-mini.local>
2022-02-22 03:52:09 +1100

    Separating out domain data into new module

--- fittracker.hs
+++ fittracker.hs
@@ -3,49 +3,7 @@ import Data.Time.Calendar ( fromGregorian )
 import Data.Time ( UTCTime(UTCTime), secondsToDiffTime )
 import Data.Map (Map)
 import qualified Data.Map as Map
-
-
-
-type RepCount = Integer
-type Weight = Double
-
-data Outcome = Success | Failure deriving (Show)
-instance Eq Outcome where
-    (==) Success Success = True
-    (==) Failure Failure = True
-    (==) _ _ = False
-instance Ord Outcome where
-    compare Success Success = EQ
-    compare Failure Failure = EQ
-    compare Success Failure = GT
-    compare Failure Success = LT
-
-data WorkoutType = FiveByFive | MadCow | UpperLowerSplit deriving (Show, Eq, Ord)
-data WorkoutSubType = WorkoutA | WorkoutB deriving (Show, Eq, Ord)
-data ExerciseType = Squat | BenchPress | Deadlift | OverheadPress | BentOverRows deriving (Show, Eq, Ord)
-data Reps = Reps {
-  weight :: Weight
-  , repCount ::  RepCount
-  , outcome :: Outcome } deriving (Show, Eq, Ord)
-
-data Exercise = Exercise {
-  exerciseType :: ExerciseType
-  , exerciseTime :: Maybe UTCTime
-  , sets :: [Reps] } deriving (Show, Eq, Ord)
-
-data Workout = Workout {
-  workoutType :: WorkoutType
-  , workoutSubType :: WorkoutSubType
-  , workoutTime :: Maybe UTCTime
-  , exercises :: [Exercise] } deriving (Show, Eq, Ord)
-
-data Database = Database {
-  exerciseLookup :: Map ExerciseType [Exercise]
-  , proposedWorkout :: Maybe Workout
-} deriving (Show, Eq, Ord)
-
-
-
+import Domain
 
 inc :: ExerciseType -> Weight -> Weight
 inc Deadlift w = w + 5
